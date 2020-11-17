97e002bc4f47162be1f47bd4bdfc5e4bcfa5c7cb
alephyv <alephyvinicius18@gmail.com>
2020-11-17 16:01:04 -0300

    primeiro commit

---
+++ ClasseAbstrataInterface/exercicio1/Car.java
@@ -0,0 +1,35 @@
+
+package l1;
+
+public class Car extends CarbonFootprint{
+    float combustivel;
+    float cilindrada;
+
+    public Car(float combustivel, float cilindrada) {
+        this.combustivel = combustivel;
+        this.cilindrada = cilindrada;
+    }
+
+    public float getCombustivel() {
+        return combustivel;
+    }
+
+    public void setCombustivel(float combustivel) {
+        this.combustivel = combustivel;
+    }
+
+    public float getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(float cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    @Override
+    public double getCarbonFootprint() {
+        double carbonFootprint = this.cilindrada + this.combustivel;
+        return carbonFootprint;
+    }
+    
+}
---
+++ ClasseAbstrataInterface/exercicio1/CarbonFootprint.java
@@ -0,0 +1,6 @@
+
+package l1;
+
+public abstract class CarbonFootprint {
+    public abstract double getCarbonFootprint();
+}
